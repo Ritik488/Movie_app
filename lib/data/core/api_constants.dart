ad505b3eaa3ff00dcaf96391101dfd1ec9b45a8a
Ritik <ritikmiglani488@gmail.com>
2021-10-02 17:43:46 +0530

    first commit

---
+++ lib/data/core/api_constants.dart
@@ -0,0 +1,7 @@
+class ApiConstants {
+  ApiConstants._();
+
+  static const String BASE_URL = "https://api.themoviedb.org/3/";
+  static const String API_KEY = "b1b462878d236f01964e540ffc1038e3";
+  static const String BASE_IMG_URL = 'https://image.tmdb.org/t/p/w500';
+}

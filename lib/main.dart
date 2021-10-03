c24e7696a37036d5cf8a32d1cf56a266854f5737
Ritik <ritikmiglani488@gmail.com>
2021-10-03 15:20:04 +0530

    adding dependency injection

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,15 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'package:movie_app/data/core/api_client.dart';
-import 'package:movie_app/data/data_sources/movie_remote_data_source.dart';
-import 'package:movie_app/data/repositories/movie_repository_impl.dart';
 import 'package:movie_app/domain/entities/app_error.dart';
 import 'package:movie_app/domain/entities/movie_entity.dart';
 import 'package:movie_app/domain/entities/no_params.dart';
-import 'package:movie_app/domain/repositories/movie_repositories.dart';
 import 'package:movie_app/domain/usecases/get_trending.dart';
+import 'package:pedantic/pedantic.dart';
+import 'di/get_it.dart' as getIt;
 
 void main() async {
-  Client apiClient = Client();
-  MovieRemoteDataSource dataSource =
-      MovieRemoteDataSourceImpl(ApiClient(apiClient));
-  MovieRepository movieRepository = MovieRepositoryImpl(dataSource);
-  GetTrending getTrending = GetTrending(movieRepository);
+  unawaited(getIt.init());
+  GetTrending getTrending = getIt.getItInstance<GetTrending>();
   final Either<AppError, List<MovieEntity>> eitherResponse =
       await getTrending(NoParams());
   eitherResponse.fold((l) {
